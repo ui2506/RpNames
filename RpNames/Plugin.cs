1121f2c2f871e15b6e2eae401ec0693796f147f9
ui <main@ui-drem.com>
2026-04-20 22:10:22 +0500

    asdasd

--- RpNames/Plugin.cs
+++ RpNames/Plugin.cs
@@ -9,7 +9,7 @@ namespace RpNames
         public override string Prefix { get; } = "RpNames";
         public override string Name { get; } = "RpNames";
         public override string Author { get; } = "ui_2506";
-        public override Version Version { get; } = new Version(1, 3, 1);
+        public override Version Version { get; } = new Version(1, 3, 2);
 
         internal static Config PluginConfig { get; private set; }
 
